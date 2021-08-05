8e18a45957a839bcd5b6956495387636a3fbb21b
Nivek218 <kevinenrique_01@hotmail.com>
2021-08-05 00:36:42 -0500

    Avance No.6
    Buenas tardes profesor, en esta parte que estoy subiendo se anexo el service, controller y la interface.

---
+++ ineg/src/main/java/com/ineg/ineg/repository/IFlujoRepository.java
@@ -0,0 +1,7 @@
+package com.ineg.ineg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IFlujoRepository extends JpaRepository <flujo,Long
+        > {
+}
---
+++ ineg/src/main/java/com/ineg/ineg/services/FlujoService.java
@@ -0,0 +1,13 @@
+package com.ineg.ineg.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FlujoService {
+    public List getlistFromDB() {
+        return
+    }
+}
+
